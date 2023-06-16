e75ea92eec002ce8341f17c024794bfbc14df2d3
AAAAA <AAAAA@gmail.com>
2023-06-16 19:14:10 +0100

    .

---
+++ 1-Programing/Python/100_days_of_code/Exercises/25_csv/main.py
@@ -0,0 +1,12 @@
+# import csv
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for i in data:
+#         if i[1] != "temp":
+#             temperatures.append(int(i[1]))
+    
+#     print(temperatures)
+
+import pandas
