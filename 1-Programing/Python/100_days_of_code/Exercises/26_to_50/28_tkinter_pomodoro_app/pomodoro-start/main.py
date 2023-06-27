6d07ba25a7ec55238fb32607f9e6c85b74b541fd
AAAAA <AAAAA@gmail.com>
2023-06-27 17:24:48 +0100

    .

--- 1-Programing/Python/100_days_of_code/Exercises/26_to_50/28_tkinter_pomodoro_app/pomodoro-start/main.py
+++ 1-Programing/Python/100_days_of_code/Exercises/26_to_50/28_tkinter_pomodoro_app/pomodoro-start/main.py
@@ -7,57 +7,70 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 25   # The program works for 25 minutes
+SHORT_BREAK_MIN = 5  # After 25 minutes it goes to a 5 min break
+LONG_BREAK_MIN = 20 # The app repeats 4 times, so (25 min work, 5 min break) x4 and then it takes 20 min break
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-def reset():
-    print("Reset Button")
+def reset_timer():
+    root.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    my_label.config(text="Timer")
+    my_label_check_marks.config(text="")
+    global reps
+    reps = 0
 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM (START TIMER)------------------------------- # 
 def start_timer():
     global reps
     reps = 0
 
-    work_sec = WORK_MIN * 60 
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
+    work_sec = WORK_MIN * 60 # We convert the 25 min to seconds, 1500 secs
+    short_break_sec = SHORT_BREAK_MIN * 60 # Convert 5 min to seconds, 300 secs
+    long_break_sec = LONG_BREAK_MIN * 60 # Convert 20 min to seconds, 1200 secs
     
-    if reps % 8 == 0:
+    if reps % 8 == 0: # 20 min BREAK. If the numbere of breaks divided by 0 has no reminder is time for a long break
         count_down(long_break_sec)
         my_label.config(text="Break", fg=RED)
-    elif reps % 2 == 0:
+    elif reps % 2 == 0: # BREAK 5 minutes. If reps is even it's time for a short break
         count_down(short_break_sec)
         my_label.config(text="Break", fg=PINK)
-    else:
+    else:  # If it's none of the previus conditions it's normal work time
         count_down(work_sec)
         my_label.config(text="Work", fg=GREEN)
 
 
-    # count_down(1500)  # We call the method that will show the count down into the tomato label
+   
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-def count_down(count):
+def count_down(count): # This function updates the timer display by converting the reminder time  into minutes and seconds
       
-    count_min = math.floor(count / 60)
+    count_min = math.floor(count / 60) # math.floor rounds down to the nearest integer, giving you a whole number of minutes
     count_sec = count % 60
 
-    if count_sec == 0:
+    if count_sec == 0: # Without this will show the seconds with one 0 only instead of two when is necessary, like 25:0 to 25:00
             count_sec = "00"
 
-    # elif count_sec < 0:   # Without this will show the seconds with one 0 only instead of two when is necessary, like 25:0 to 25:00
-    #         count_sec = f"0{count_sec}" # so we use Dynamic Typing to pass from integer to string.Is just changing the type of the variable.
+    # count_sec = f"0{count_sec}" # so we use Dynamic Typing to pass from integer to string.Is just changing the type of the variable.
       
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}" ) #This updates the timer display with the minues and secods. Itemconfig changes a canvas element.
+    #And you want to change text="00:00" for the variable count
     
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}" ) # itemconfig changes a canvas element. And you want to change text="00:00" for the variable count
-    if count > 0:
-        root.after(1000, count_down, count - 1)
-    else:
-         start_timer()
 
+    if count > 0: # If there is remaining time, it schedules the next iteration of the coutdown with 1 second delay.
+        global timer
+        timer = root.after(1000, count_down, count - 1) # After waits to execute the function x time, 1000 milisecs in this example, which is 1 sec.
+    else:
+        start_timer() # If count is not greater than 0 the countdown has reached 0 and stat_timer() function is triggered.
+        marks = ""
+        work_sessions = math.floor(reps/2) # This creates a check mark, ✔, for every 25 minutes. You will have 4 marks and then you will have a 20 min break
+        for i in range(work_sessions):
+            marks += "✔"
+        my_label_check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Use Tkinter to create a new window, run it and put a background color
@@ -81,11 +94,11 @@ button_start = Button(text="Start", highlightbackground=YELLOW, command=start_ti
 button_start.grid(row=2, column=0) 
 
 #Button Reset
-button_reset = Button(text="Reset", highlightbackground=YELLOW, command=reset)
+button_reset = Button(text="Reset", highlightbackground=YELLOW, command=reset_timer)
 button_reset.grid(row=2, column=2)
 
 # Label check marks
-my_label_check_marks = Label(text="✓", bg=YELLOW, fg=GREEN)
+my_label_check_marks = Label(bg=YELLOW, fg=GREEN)
 my_label_check_marks.grid(row=3,column=1 )
 
 root.mainloop()
