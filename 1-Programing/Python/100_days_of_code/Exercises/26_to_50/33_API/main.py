e1e854c6be47ca58d9832453aa75361d42e45ac5
AAAAA <AAAAA@gmail.com>
2023-08-02 14:06:23 +0100

    .

--- 1-Programing/Python/100_days_of_code/Exercises/26_to_50/33_API/main.py
+++ 1-Programing/Python/100_days_of_code/Exercises/26_to_50/33_API/main.py
@@ -1,4 +1,32 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+MY_LAT = 55.953251
+MY_LONG = -3.188267
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# longitude = response.json()["iss_position"]["longitude"]
+# latitude = response.json()["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(sunrise)
+
+time_now = datetime.now()
+
+print(time_now)
